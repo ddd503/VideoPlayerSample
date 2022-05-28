22824064117b227f238502158d6b80a7ecbf677e
Dai Kawahara <skbsporta7@gmail.com>
2022-05-28 20:55:27 +0900

    add avplayer observe

--- VideoPlayerSample/PlayerViewPresenter.swift
+++ VideoPlayerSample/PlayerViewPresenter.swift
@@ -8,42 +8,52 @@
 import Foundation
 import AVFoundation
 
-enum ViewStatus {
-    case play
-    case pause
-}
-
 protocol PlayViewOutput: AnyObject {
     func setupPlayerView(avPlayer: AVPlayer)
     func updatePlayButtonImage(imageName: String)
 }
 
 final class PlayerViewPresenter {
-    private var viewStatus: ViewStatus = .pause
     private let videoUrl: URL
-    private let avPlayer = AVPlayer()
+    private let avPlayer: AVPlayer
+    private var playerTimeControlStatusObserver: NSKeyValueObservation?
     var output: PlayViewOutput?
 
-    init(videoUrl: URL) {
+    init(videoUrl: URL, avPlayer: AVPlayer = AVPlayer()) {
         self.videoUrl = videoUrl
+        self.avPlayer = avPlayer
     }
 
     func viewDidLoad() {
         let asset = AVURLAsset(url: videoUrl)
         avPlayer.replaceCurrentItem(with: AVPlayerItem(asset: asset))
+        setupPlayerObservers(avPlayer)
         output?.setupPlayerView(avPlayer: avPlayer)
     }
 
     func didTapPlayButton() {
-        switch viewStatus {
-        case .play:
-            avPlayer.pause()
-            output?.updatePlayButtonImage(imageName: "play.circle.fill")
-            viewStatus = .pause
-        case .pause:
+        switch avPlayer.timeControlStatus {
+        case .paused, .waitingToPlayAtSpecifiedRate:
             avPlayer.play()
-            output?.updatePlayButtonImage(imageName: "pause.circle.fill")
-            viewStatus = .play
+        case .playing:
+            avPlayer.pause()
+        @unknown default:
+            avPlayer.pause()
+        }
+    }
+
+    private func setupPlayerObservers(_ avPlayer: AVPlayer) {
+        // 再生中のプレイヤー状態を監視し、再生ボタンのイメージを切り替える
+        playerTimeControlStatusObserver = avPlayer.observe(\AVPlayer.timeControlStatus,
+                                                            options: [.initial, .new]) { [weak self] observeAvPlayer, value in
+            switch observeAvPlayer.timeControlStatus {
+            case .paused, .waitingToPlayAtSpecifiedRate:
+                self?.output?.updatePlayButtonImage(imageName: "play.circle.fill")
+            case .playing:
+                self?.output?.updatePlayButtonImage(imageName: "pause.circle.fill")
+            @unknown default:
+                self?.output?.updatePlayButtonImage(imageName: "pause.circle.fill")
+            }
         }
     }
 }
